3faec2cf55899f9654ac87b54edcbdb57ada1989
Cj <55663953+CharliePine22@users.noreply.github.com>
2022-05-18 17:20:21 -0500

    Projects section start
    Finished up the about section styling and altered the Navbar component slightly. Starting the projects component now.

--- src/components/PortfolioPage/Portfolio.js
+++ src/components/PortfolioPage/Portfolio.js
@@ -4,9 +4,16 @@ const Portfolio = () => {
   return (
     <>
       <div id="portfolio" className={styles.container}>
-        <div className={styles["portfolio-description"]}>
-          <div className={styles.title}>
-            <h3>Portfolio</h3>
+        <div className={styles["portfolio-content"]}>
+          <h3>Portfolio</h3>
+
+          <div className={styles["portfolio-description"]}>
+            <p>Here are some of the projects that I've worked on!</p>
+          </div>
+          <div className={styles.projects}>
+              <div className={styles['projects-nav']}>
+
+              </div>
           </div>
         </div>
       </div>
