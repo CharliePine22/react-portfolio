4183ba270e0092dec98783a6801aa3517b0f05c7
Cj <55663953+CharliePine22@users.noreply.github.com>
2023-12-06 17:39:26 -0600

    Reamped Portfolio section design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react';
-import About from './components/AboutPage/About';
-import Contact from './components/ContactPage/Contact';
-import Home from './components/HomePage/Home';
-import Navbar from './components/Navbar/Navbar';
-import Portfolio from './components/PortfolioPage/Portfolio';
-import Skills from './components/Skills/Skills';
-import Landing from './components/LandingAnimation/Landing';
-import './index.css';
+import React, { useEffect } from "react";
+import About from "./components/AboutPage/About";
+import Contact from "./components/ContactPage/Contact";
+import Home from "./components/HomePage/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Portfolio from "./components/PortfolioPage/Portfolio";
+import PortfolioNew from "./components/PortfolioPage/PortfolioNew";
+import Skills from "./components/Skills/Skills";
+import Landing from "./components/LandingAnimation/Landing";
+import "./index.css";
 
 function App() {
   // Scroll to top of window on refresh to view animation
@@ -21,12 +22,13 @@ function App() {
   return (
     <>
       <Navbar />
-      <Home id='home' />
+      <Home id="home" />
 
-      <Portfolio id='portfolio' />
-      <Skills id='skills' />
-      <About id='about' />
-      <Contact id='contact' />
+      {/* <Portfolio id="portfolio" /> */}
+      <PortfolioNew id="portfolio" />
+      <Skills id="skills" />
+      <About id="about" />
+      <Contact id="contact" />
     </>
   );
 }
